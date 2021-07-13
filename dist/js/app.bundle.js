c987d0b28a3e5fdc3ee72f9016f543aab2908319
murtaza akhtar <42249079+murtaza135@users.noreply.github.com>
2021-07-13 13:40:01 +0100

    created base markup, style and javascript for bookmarks modal, used to create a new bookmark

--- dist/js/app.bundle.js
+++ dist/js/app.bundle.js
@@ -1,13 +1,19 @@
 const settingsButton = document.querySelector("#settings");
 const settingsModal = document.querySelector("#settings-modal");
-const modalContent = document.querySelector(".modal-content");
-const modalCloseBtn = document.querySelector(".modal-content .modal-close-btn");
+const settingsModalContent = document.querySelector("#settings-modal .modal-content");
+const settingsModalCloseBtn = document.querySelector("#settings-modal .modal-content .modal-close-btn");
+
+const templateCard = document.querySelector(".template-card");
+const bookmarksModal = document.querySelector("#bookmarks-modal");
+const bookmarksModalContent = document.querySelector("#bookmarks-modal .modal-content");
+const bookmarksModalCloseBtn = document.querySelector("#bookmarks-modal .modal-content .modal-close-btn");
+
 
 settingsButton.addEventListener("click", openSettingsModal);
 settingsModal.addEventListener("click", closeSettingsModal);
-modalCloseBtn.addEventListener("click", closeSettingsModal);
+settingsModalCloseBtn.addEventListener("click", closeSettingsModal);
 
-function openSettingsModal(event) {
+function openSettingsModal() {
     settingsModal.style.display = "block";
 }
 
@@ -15,7 +21,25 @@ function closeSettingsModal(event) {
     if (event.target === settingsModal) {
         settingsModal.style.display = "none";
     }
-    else if (event.target.parentElement === modalCloseBtn) {
+    else if (event.target.parentElement === settingsModalCloseBtn) {
         settingsModal.style.display = "none";
+    }
+}
+
+
+templateCard.addEventListener("click", openBookmarksModal);
+bookmarksModal.addEventListener("click", closeBookmarksModal);
+bookmarksModalCloseBtn.addEventListener("click", closeBookmarksModal);
+
+function openBookmarksModal() {
+    bookmarksModal.style.display = "block";
+}
+
+function closeBookmarksModal(event) {
+    if (event.target === bookmarksModal) {
+        bookmarksModal.style.display = "none";
+    }
+    else if (event.target.parentElement === bookmarksModalCloseBtn) {
+        bookmarksModal.style.display = "none";
     }
 }
