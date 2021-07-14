37ffef32abf0ba67f6cddf98ff2d4b1beb483eca
murtaza akhtar <42249079+murtaza135@users.noreply.github.com>
2021-07-14 18:31:30 +0100

    cleaned up js and scss for both muuri DnD and vanilla js Dnd in settings modal

--- dist/js/app.bundle.js
+++ dist/js/app.bundle.js
@@ -21,15 +21,16 @@ function closeSettingsModal(event) {
     if (event.target === settingsModal) {
         settingsModal.style.display = "none";
     }
-    else if (event.target.parentElement === settingsModalCloseBtn) {
+    else if (event.target.parentElement === settingsModalCloseBtn
+            || event.target === settingsModalCloseBtn) {
         settingsModal.style.display = "none";
     }
 }
 
 
-// templateCard.addEventListener("click", openBookmarksModal);
-// bookmarksModal.addEventListener("click", closeBookmarksModal);
-// bookmarksModalCloseBtn.addEventListener("click", closeBookmarksModal);
+templateCard.addEventListener("click", openBookmarksModal);
+bookmarksModal.addEventListener("click", closeBookmarksModal);
+bookmarksModalCloseBtn.addEventListener("click", closeBookmarksModal);
 
 function openBookmarksModal() {
     bookmarksModal.style.display = "block";
@@ -39,154 +40,80 @@ function closeBookmarksModal(event) {
     if (event.target === bookmarksModal) {
         bookmarksModal.style.display = "none";
     }
-    else if (event.target.parentElement === bookmarksModalCloseBtn) {
+    else if (event.target.parentElement === bookmarksModalCloseBtn
+            || event.target === bookmarksModalCloseBtn) {
         bookmarksModal.style.display = "none";
     }
 }
 
 
+// Draggable Bookmarks List in Settings Modal (https://www.youtube.com/watch?v=jfYWwQrtzzY)
+const draggableBookmarkListElements = document.querySelectorAll(".bookmarks-list-element");
+const bookmarksList = document.querySelector(".bookmarks-list");
 
-// Draggable (https://www.youtube.com/watch?v=jfYWwQrtzzY)
-// const draggables = document.querySelectorAll(".bookmarks-list-element");
-// const bookmarksList = document.querySelector(".bookmarks-list");
+bookmarksList.addEventListener("dragover", bookmarksListDragOver);
+draggableBookmarkListElements.forEach(bookmark => {
+    bookmark.addEventListener("dragstart", bookmarksListDragStart);
+    bookmark.addEventListener("dragend", bookmarksListDragEnd);
+})
 
-// draggables.forEach(draggable => {
-//     draggable.addEventListener("dragstart", event => dragStart(event, draggable));
-//     draggable.addEventListener("dragend", event => dragEnd(event, draggable));
-// })
+function bookmarksListDragStart() {
+    console.log(this);
+    this.classList.add("dragging");
+}
 
-// function dragStart(event, draggable) {
-//     draggable.classList.add("dragging");
-// }
+function bookmarksListDragEnd() {
+    this.classList.remove("dragging");
+}
 
-// function dragEnd(event, draggable) {
-//     draggable.classList.remove("dragging");
-// }
+function bookmarksListDragOver(event) {
+    event.preventDefault();
+    const draggable = document.querySelector(".dragging");
+    console.log(draggable);
+    const afterElement = getDragAfterElement(bookmarksList, event.clientY);
+    if (afterElement == null) {
+        bookmarksList.appendChild(draggable);
+    }
+    else {
+        bookmarksList.insertBefore(draggable, afterElement);
+    }
+}
 
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll(".bookmarks-list-element:not(.dragging)")];
+    const closestElement = draggableElements.reduce((closest, draggable) => {
+        const box = draggable.getBoundingClientRect();
+        const offset = y - (box.top + (box.height / 2));
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: draggable };
+        }
+        else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY });
 
-// bookmarksList.addEventListener("dragover", dragOver);
-
-// function dragOver(event) {
-//     event.preventDefault();
-//     const draggable = document.querySelector(".dragging");
-//     const afterElement = getDragAfterElement(bookmarksList, event.clientY);
-//     if (afterElement == null) {
-//         bookmarksList.appendChild(draggable);
-//     }
-//     else {
-//         bookmarksList.insertBefore(draggable, afterElement);
-//     }
-// }
-
-// function getDragAfterElement(container, y) {
-//     const draggableElements = [...container.querySelectorAll(".bookmarks-list-element:not(.dragging)")];
-//     const closestElement = draggableElements.reduce((closest, draggable) => {
-//         const box = draggable.getBoundingClientRect();
-//         const offset = y - (box.top + (box.height / 2));
-//         if (offset < 0 && offset > closest.offset) {
-//             return { offset: offset, element: draggable };
-//         }
-//         else {
-//             return closest;
-//         }
-//     }, { offset: Number.NEGATIVE_INFINITY });
-
-//     return closestElement.element;
-// }
-
-
-// Draggables 2
-// const draggableBookmarks = document.querySelectorAll(".bookmarks-card");
-// const bookmarksGrid = document.querySelector("#bookmarks-grid");
-
-// draggableBookmarks.forEach(draggable => {
-//     draggable.addEventListener("dragstart", event => dragStart(event, draggable));
-//     draggable.addEventListener("dragend", event => dragEnd(event, draggable));
-// })
-
-// function dragStart(event, draggable) {
-//     draggable.classList.add("dragging");
-// }
-
-// function dragEnd(event, draggable) {
-//     draggable.classList.remove("dragging");
-// }
+    return closestElement.element;
+}
 
 
-// bookmarksGrid.addEventListener("dragover", dragOver);
-
-// function dragOver(event) {
-//     event.preventDefault();
-//     const draggable = document.querySelector(".dragging");
-//     const draggedOverElement = getDraggedOverElement(bookmarksGrid, event.clientX, event.clientY);
-//     console.log(draggedOverElement);
-//     if (draggedOverElement == null) {
-//         bookmarksGrid.insertBefore(draggable, templateCard);
-//     }
-//     else {
-//         bookmarksGrid.insertBefore(draggable, draggedOverElement);
-//     }
-// }
-
-// function getDraggedOverElement(container, x, y) {
-//     const draggableElements = [...container.querySelectorAll(".bookmarks-card:not(.dragging)")];
-//     const draggedOverElement = draggableElements.reduce((current, draggable) => {
-//         const box = draggable.getBoundingClientRect();
-//         offsetX = x - (box.left + (box.width / 2));
-//         offsetY = y - (box.top + (box.height / 2));
-//         offset = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2));
-//         if (y > box.y && y < box.y + box.height && x > box.x && x < box.x + box.width && offset > current.offset) {
-//             return { offset: offset, element: draggable };
-//         }
-//         else {
-//             return current;
-//         }
-//     }, { offset: Number.NEGATIVE_INFINITY })
-
-//     return draggedOverElement.element;
-// }
-
-
-// function getDragAfterElement(container, y) {
-//     const draggableElements = [...container.querySelectorAll(".bookmarks-list-element:not(.dragging)")];
-//     const closestElement = draggableElements.reduce((closest, draggable) => {
-//         const box = draggable.getBoundingClientRect();
-//         const offset = y - (box.top + (box.height / 2));
-//         if (offset < 0 && offset > closest.offset) {
-//             return { offset: offset, element: draggable };
-//         }
-//         else {
-//             return closest;
-//         }
-//     }, { offset: Number.NEGATIVE_INFINITY });
-
-//     return closestElement.element;
-// }
-
-
-// Draggable 4: Muuri
-var grid = new Muuri('#bookmarks-grid', {
+// Draggable Bookmarks Grid
+let grid = new Muuri('#bookmarks-grid', {
     items: ".bookmarks-item",
     dragEnabled: true,
     layoutOnResize: 10,
-    layoutDuration: 400,
-    layoutEasing: "ease-out",
-    dragStartPredicate: (item, event) => {
-        // Prevent template-card from being dragged 
-        if (item._element.classList.contains("template-card")) {
-            return false;
-        }
-        // For other items use the default drag start predicate.
-        return Muuri.ItemDrag.defaultStartPredicate(item, event);
-      }
+    layoutDuration: 300,
+    layoutEasing: "linear",
+    layoutOnInit: true,
+    layout: centerLayout,
+    // dragStartPredicate: makeTemplateCardNonDraggable
 });
 
-const draggables = document.querySelectorAll(".bookmarks-item");
+const draggableBookmarkItems = document.querySelectorAll(".bookmarks-item");
 
-draggables.forEach(draggable => {
-    draggable.addEventListener("click", clickBookmark);
-    draggable.addEventListener("dragstart", dragStart);
-    draggable.addEventListener("dragend", dragEnd);
+draggableBookmarkItems.forEach(bookmark => {
+    bookmark.addEventListener("click", clickBookmark);
+    bookmark.addEventListener("dragstart", bookmarksGridDragStart);
+    bookmark.addEventListener("dragend", bookmarksGridDragEnd);
 })
 
 function clickBookmark(event) {
@@ -196,10 +123,127 @@ function clickBookmark(event) {
     }
 }
 
-function dragStart() {
+function bookmarksGridDragStart() {
     this.classList.add("muuri-item-dragging-before-click");
 }
 
-function dragEnd() {
+function bookmarksGridDragEnd() {
     this.classList.remove("muuri-item-dragging-before-click");
+}
+
+function makeTemplateCardNonDraggable(item, event) {
+    // Prevent template-card from being dragged 
+    if (item._element.classList.contains("template-card")) {
+        return false;
+    }
+    // For other items use the default drag start predicate.
+    return Muuri.ItemDrag.defaultStartPredicate(item, event);
+}
+
+function centerLayout(grid, layoutId, items, width, height, callback) {
+    let layout = {
+        id: layoutId,
+        items: items,
+        slots: [],
+        styles: {},
+    };
+
+    let item;
+    let m;
+    let x = 0;
+    let y = 0;
+    let w = 0;
+    let h = 0;
+
+    let maxW = width / 2;
+    let currentW = 0;
+    let currentRowH = 0;
+    let currentRowW = 0;
+    let rowSizes = [];
+    let rowFixes = [];
+
+    let xPre, yPre, wPre, hPre;
+    let numToFix = 0;
+
+    for (let i = 0; i < items.length; i++) {
+        item = items[i];
+
+        m = item.getMargin();
+        wPre = item.getWidth() + m.left + m.right;
+        hPre = item.getHeight() + m.top + m.bottom;
+        xPre += wPre;
+
+        if (hPre > currentRowH) {
+            currentRowH = hPre;
+        }
+
+        if (w < currentRowW) {
+            currentRowW = wPre;
+        }
+
+        rowSizes.push(width / 2);
+        numToFix++;
+        currentW += wPre;
+
+        let k = 0;
+
+        for (let j = 0; j < numToFix; j++) {
+            rowSizes[i - j] -= wPre / 2;
+        }
+
+        if (numToFix > 1) {
+            rowSizes[i] -= (wPre / 2) * (numToFix - 1);
+            k += (wPre / 2);
+        }
+
+        currentW -= k;
+        rowFixes.push(k);
+
+        if (currentW >= maxW) {
+            yPre += currentRowH;
+            currentRowH = 0;
+            xPre = 0;
+            numToFix -= 1;
+            currentW = 0;
+            numToFix = 0;
+            k = 0;
+        }
+    }
+
+    maxW = width / 2;
+    currentW = 0;
+    currentRowH = 0;
+    currentRowW = 0;
+
+    for (let i = 0; i < items.length; i++) {
+        item = items[i];
+        x += w;
+
+        if (h > currentRowH) {
+            currentRowH = h;
+        }
+
+        if (w < currentRowW) {
+            currentRowW = w;
+        }
+
+        currentW += w - rowFixes[i];
+
+        if (currentW >= maxW) {
+            y += currentRowH;
+            currentRowH = 0;
+            x = 0;
+            currentW = 0;
+        }
+
+        m = item.getMargin();
+        w = item.getWidth() + m.left + m.right;
+        h = item.getHeight() + m.top + m.bottom;
+        layout.slots.push(x + rowSizes[i], y);
+    }
+
+    layout.styles.width = '100%';
+    layout.styles.height = y + h + 1 + 'px';
+
+    callback(layout);
 }
