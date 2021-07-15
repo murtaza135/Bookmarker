e26e82fd681a5eaf3845fe4c2c5821d9f46fd544
murtaza akhtar <42249079+murtaza135@users.noreply.github.com>
2021-07-16 00:38:04 +0100

    added ability to add a DateTimeBookmark and a TemplateBookmark to bookmarks_controller.js

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,10 +1,28 @@
-export default class Bookmark {
-    constructor(id, name, url, description, image=null, isVisible = true) {
+export class BaseBookmark {
+    constructor(id, name, isVisible = true) {
         this.id = id;
         this.name = name;
+        this.isVisible = isVisible;
+    }
+}
+
+export class Bookmark extends BaseBookmark {
+    constructor(id, name, url, description, image, isVisible = true) {
+        super(id, name, isVisible);
         this.url = url;
         this.description = description;
         this.image = image;
-        this.isVisible = isVisible;
+    }
+}
+
+export class DateTimeBookmark extends BaseBookmark {
+    constructor(id, name, isVisible = true) {
+        super(id, name, isVisible);
+    }
+}
+
+export class TemplateBookmark extends BaseBookmark {
+    constructor(id, name, isVisible = true) {
+        super(id, name, isVisible);
     }
 }
--- js/bookmarks_controller.js
+++ js/bookmarks_controller.js
@@ -1,12 +1,23 @@
-import Bookmark from "./bookmark";
+import { Bookmark, DateTimeBookmark, TemplateBookmark } from "./bookmark";
 
 export default class BookmarksController {
     constructor(bookmarks = []) {
         this._bookmarks = [];
+        this._createInitialBookmarks();
 
         bookmarks.forEach(bookmark => {
             this.reAddBookmark(bookmark);
         })
+    }
+
+    _createInitialBookmarks() {
+        let id = this._generateNewId()
+        const dateTimeBookmark = new DateTimeBookmark(id, "Date Time", true);
+        this._bookmarks.push(dateTimeBookmark);
+        
+        id = this._generateNewId()
+        const templateBookmark = new TemplateBookmark(id, "Date Time", true);
+        this._bookmarks.push(templateBookmark);
     }
 
     reAddBookmark(newBookmark, overwriteOld = true) {
@@ -46,9 +57,16 @@ export default class BookmarksController {
     }
 
     addNewBookmark(name, url, description, image, isVisible = true) {
-        id = this._generateNewId();
+        const id = this._generateNewId();
         const bookmark = new Bookmark(id, name, url, description, image, isVisible);
         this._bookmarks.push();
+        return bookmark;
+    }
+
+    addCustomBookmark(bookmarkType, name, isVisible = true) {
+        const id = this._generateNewId();
+        const bookmark = new bookmarkType(id, name, isVisible);
+        this._bookmarks.push(bookmark);
         return bookmark;
     }
 
@@ -96,6 +114,6 @@ export default class BookmarksController {
     moveBookmark(id, toIndex) {
         const bookmarkIndex = this.getBookmarkIndex(id);
         const bookmark = this._bookmarks.splice(bookmarkIndex, 1)[0];
-        this._bookmarks.splice(toIndex, 0, bookmark);
+        return this._bookmarks.splice(toIndex, 0, bookmark)[0];
     }
 }
