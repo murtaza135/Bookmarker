88d0acea070d9d894878fc919f8bfac33c17d261
murtaza akhtar <42249079+murtaza135@users.noreply.github.com>
2021-07-16 01:55:14 +0100

    added all needed UI selectors

--- js/ui.js
+++ js/ui.js
@@ -1,24 +1,28 @@
 export default class UI {
     constructor() {
         // Bookmarks grid on main page
-        const bookmarksGrid = document.querySelector("#bookmarks-grid");
-        const bookmarkItemsAll = () => document.querySelector("#bookmarks-grid .bookmarks-item");
+        this.bookmarksGrid = document.querySelector("#bookmarks-grid");
+        this.bookmarkItemSelector = "#bookmarks-grid .bookmarks-item";
 
         // Bookmarks modal for adding new bookmarks
-        const templateCard = () => document.querySelector("#template-card");
-        const bookmarksModal = document.querySelector("#bookmarks-modal");
-        const bookmarksModalCloseBtn = () => document.querySelector("#bookmarks-modal .modal-close-btn");
-        const bookmarksNameInput = () => document.querySelector("#bookmarks-name-input");
-        const bookmarksUrlInput = () => document.querySelector("#bookmarks-url-input");
-        const bookmarksDescriptionInput = () => document.querySelector("#bookmarks-description-input");
-        const bookmarksImageUploadText = () => document.querySelector("#bookmarks-image-upload-text");
-        const bookmarksImageInput = () => document.querySelector("#bookmarks-image-input");
-        const bookmarksSubmitBtn = () => document.querySelector(".bookmarks-submit-btn");
+        this.templateCardSelector = "#template-card";
+        this.bookmarksModal = document.querySelector("#bookmarks-modal");
+        this.bookmarksModalCloseBtn = document.querySelector("#bookmarks-modal .modal-close-btn");
+        this.bookmarksNameInput = document.querySelector("#bookmarks-name-input");
+        this.bookmarksUrlInput = document.querySelector("#bookmarks-url-input");
+        this.bookmarksDescriptionInput = document.querySelector("#bookmarks-description-input");
+        this.bookmarksImageUploadText = document.querySelector("#bookmarks-image-upload-text");
+        this.bookmarksImageInput = document.querySelector("#bookmarks-image-input");
+        this.bookmarksSubmitBtn = document.querySelector(".bookmarks-submit-btn");
 
         // Settings modal
-        const settingsButton = document.querySelector("#settings");
-        const settingsModal = document.querySelector("#settings-modal");
-        const settingsModalCloseBtn = () => document.querySelector("#settings-modal .modal-close-btn");
-        const settingsBookmarksList = () => document.querySelector("#settings-modal .bookmarks-list");
+        this.settingsButton = document.querySelector("#settings");
+        this.settingsModal = document.querySelector("#settings-modal");
+        this.settingsModalCloseBtn = document.querySelector("#settings-modal .modal-close-btn");
+        this.settingsBookmarksList = document.querySelector("#settings-modal .bookmarks-list");
+        this.bookmarkListElementSelector = "#settings-modal .bookmarks-list-element";
+        this.deleteBookmarkBtnSelector = "#settings-modal .bookmarks-list-element btn-delete-bookmark";
+        this.editBookmarkBtnSelector = "#settings-modal .bookmarks-list-element btn-edit-bookmark";
+        this.showBookmarkBtnSelector = "#settings-modal .bookmarks-list-element btn-show-bookmark input[type='checkbox']";
     }
 }
